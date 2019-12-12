a99c57075eb0c62b77a4acbfbbc3a92d42624319
DanPHP7 <danphp7@gmail.com>
2019-12-12 20:04:35 -0300

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
-
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Logo from '../../assets/images/logo.png';
+import {
+  Container,
+  LogoImage,
+  RedirectHome,
+  RedirectCart,
+  CartCount,
+} from './styles';
 
 export default function Header() {
-  return <Container />;
+  return (
+    <Container>
+      <RedirectHome>
+        <LogoImage source={Logo} />
+      </RedirectHome>
+      <RedirectCart>
+        <Icon name="shopping-basket" size={25} color="#FFF" />
+        <CartCount>3</CartCount>
+      </RedirectCart>
+    </Container>
+  );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,3 +1,29 @@
 import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 15px;
+  height: 50px;
+`;
+
+export const LogoImage = styled.Image`
+  height: 35;
+`;
+
+export const RedirectHome = styled(RectButton)``;
+export const RedirectCart = styled(RectButton)`
+  align-items: center;
+`;
+
+export const CartCount = styled.Text`
+  width: 17px;
+  height: 17px;
+  border-radius: 10px;
+  top: -8px;
+  color: #fff;
+  background: #04d361;
+  text-align: center;
+`;
